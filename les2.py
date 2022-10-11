c4be8beff2de7878d57acfb6b50eed27b946760c
JanaAleseeva <Jana-A_V93@mail.ru>
2022-10-11 17:37:48 +0300

    переменные

---
+++ les2.py
@@ -0,0 +1,25 @@
+a1 = 'привет'
+b1 = 'hello'
+a2 = 55
+b2 = 1
+a3 = 14.69
+b3 = 1.369
+a4 = None
+b4 = None
+a5 = True
+b5 = True
+a6 = False
+b6 = False
+print (a1)
+print (b1)
+print (a2)
+print (b2)
+print (a3)
+print (b3)
+print (a4)
+print (b4)
+print (a5)
+print (b5)
+print (a6)
+print (b6)
+
